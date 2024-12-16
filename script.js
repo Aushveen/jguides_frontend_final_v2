eae88e7b59182b3bbeb8c36dd9636a9ab35f0bb3
Aushveen Vimalathas <aushveen@gmail.com>
2024-12-15 23:07:48 -0600

    Product update

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let btn = document.getElementsByClassName("btn");
 
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
-        productImg.src = `./images/${i + 1}.jpg`;
+        //productImg.src = `./images/${i + 1}.jpg`;
+        //console.log(productImg.src.split('/').pop());
         removeActiveClass();
         this.classList.add("active");
     });
